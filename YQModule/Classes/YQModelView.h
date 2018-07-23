27ffc18abc5ce61be80e21589a221b492ef01ff5
yingqiu huang <me@huangyingqiu.com>
2018-07-23 23:09:27 +0800

    udpate1

---
+++ YQModule/Classes/YQModelView.h
@@ -0,0 +1,12 @@
+//
+//  YQModelView.h
+//  Pods-YQModule_Example
+//
+//  Created by yingqiu huang on 2018/7/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YQModelView : UIView
+
+@end
---
+++ YQModule/Classes/YQModelView.m
@@ -0,0 +1,20 @@
+//
+//  YQModelView.m
+//  Pods-YQModule_Example
+//
+//  Created by yingqiu huang on 2018/7/23.
+//
+
+#import "YQModelView.h"
+
+@implementation YQModelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
